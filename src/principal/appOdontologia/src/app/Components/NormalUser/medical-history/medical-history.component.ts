df21c2a94aeb951de41394ca48a3ec7f8902d63e
AndresMorocho <109705281+KAndresMR@users.noreply.github.com>
2024-07-27 18:33:23 -0500

    Firebase Hosting

--- src/principal/appOdontologia/src/app/Components/NormalUser/medical-history/medical-history.component.ts
+++ src/principal/appOdontologia/src/app/Components/NormalUser/medical-history/medical-history.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterLink, RouterOutlet } from '@angular/router';
+
 
 @Component({
   selector: 'app-medical-history',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule, RouterLink, RouterOutlet],
   templateUrl: './medical-history.component.html',
   styleUrl: './medical-history.component.scss'
 })
-export class MedicalHistoryComponent {
+export class MedicalHistoryComponent{
+
+
+  constructor() { }
+
+  
+
 
 }
--- src/principal/appOdontologia/src/app/Components/NormalUser/send-query/send-query.component.ts
+++ src/principal/appOdontologia/src/app/Components/NormalUser/send-query/send-query.component.ts
@@ -1,9 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-send-query',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule, RouterLink, RouterOutlet],
   templateUrl: './send-query.component.html',
   styleUrl: './send-query.component.scss'
 })
